81291c23377253176c1ee5a04d8cda25ef372995
Eliot Short <eliotshort@Eliots-MacBook-Pro-2.local>
2022-07-09 15:04:08 +0100

    and another one

--- components/ProjectCarousel.tsx
+++ components/ProjectCarousel.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import ProjectDetails from './ProjectDetails';
+// import ProjectDetails from './ProjectDetails';
 
 const settings = {
     centerMode: true,
